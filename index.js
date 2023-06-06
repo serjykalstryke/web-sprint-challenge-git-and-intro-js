534472895848c5e70cd1be6f8e000a49c467c1ef
David Stinnett <davidstinnett@icloud.com>
2023-06-05 20:58:04 -0400

    passing all tests

--- index.js
+++ index.js
@@ -288,6 +288,9 @@ function addArtist(artistArray, name, years, genre, nationality, bio) {
     nationality: nationality,
     bio: bio,
   };
+
+  copiedArray.push(newArtist);
+  return copiedArray;
 }
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -317,11 +320,15 @@ Use artistInfo to do the following:
   "Frida Kahlo de Rivera (Spanish pronunciation: [ˈfɾiða ˈkalo]; born Magdalena Carmen Frida Kahlo y Calderón; 6 July 1907 – 13 July 1954) was a Mexican artist who painted many portraits, self-portraits and works inspired by the nature and artifacts of Mexico. Inspired by the country's popular culture, she employed a naïve folk art style to explore questions of identity, postcolonialism, gender, class and race in Mexican society. Her paintings often had strong autobiographical elements and mixed realism with fantasy. In addition to belonging to the post-revolutionary Mexicayotl movement, which sought to define a Mexican identity, Kahlo has been described as a surrealist or magical realist.Born to a German father and a mestiza mother, Kahlo spent most of her childhood and adult life at her family home in Coyoacán, La Casa Azul, now known and publicly accessible as the Frida Kahlo Museum. She was disabled by polio as a child. Until a traffic accident at age eighteen caused lifelong pain and medical problems, she had been a promising student headed for medical school. During her recovery, she returned to her childhood hobby of art with the idea of becoming an artist."
 */
 
-function artistInfo(artistArray, artist) {
+function artistInfo(artistArray, artistName) {
   /*Your Code Here*/
-  const copiedArray = [...artistArray];
-  const artistInfo = copiedArray.find((artist) => artist.name === artist);
-  return artistInfo.bio;
+  const artist2 = artistArray.find((artist) => artist.name === artistName);
+
+  if (artist2) {
+    return artist2.bio;
+  } else {
+    return "Artist not found.";
+  }
 }
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 9: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -336,11 +343,14 @@ Use artistByCountry to do the following:
 
 function artistByCountry(artistArray, nationality) {
   /*Your Code Here*/
-  copiedArray = [...artistArray];
-  const artistNationality = copiedArray.find(
-    (artist) => artist.nationality === nationality
+  const filteredArtists = artistArray.filter(
+    (artist) =>
+      artist.nationality === nationality && !artist.nationality.includes(",")
   );
-  return artistNationality.name;
+
+  const artistNames = filteredArtists.map((artist) => artist.name);
+
+  return artistNames;
 }
 
 /* ***** END OF TASKS ***** */
